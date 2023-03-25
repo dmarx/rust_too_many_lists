518c8bcf885375149b85bc39a4281dcb08b0013d
David Marx <david.marx84@gmail.com>
2023-03-25 15:46:20 -0700

    added List::push

--- src/first.rs
+++ src/first.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 pub struct List {
     head: Link,
 }
@@ -9,11 +11,20 @@ enum Link {
 
 struct Node {
     elem: i32,
-    next: List,
+    next: Link,
 }
 
 impl List {
     pub fn new () -> Self {
         List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node= Box::new( Node {
+            elem: elem,
+            //next: self.head,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+        self.head = Link::More(new_node);
     }
 }

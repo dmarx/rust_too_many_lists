d10547be142db86049b4abd52a1f662f3bd13c81
David Marx <david.marx84@gmail.com>
2023-03-25 13:06:47 -0700

    List as struct

--- src/first.rs
+++ src/first.rs
@@ -1,9 +1,13 @@
+pub struct List {
+    head: Link,
+}
+
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
 struct Node {
     elem: i32,
     next: List,
-}
-
-pub enum List {
-    Empty,
-    More(Box<Node>),
 }

5e8311003e05e8bf6958c9b793e164f5cbc329a8
David Marx <david.marx84@gmail.com>
2023-03-25 17:01:00 -0700

    partial List::pop impl

--- src/first.rs
+++ src/first.rs
@@ -29,10 +29,12 @@ impl List {
     }
 
     pub fn pop(&mut self) -> Option<i32> {
-        match &self.head {
-            Link::Empty => {}
-            Link::More(node) => {}
-        };
-        unimplemented!();
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
     }
 }
